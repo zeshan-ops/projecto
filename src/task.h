9323f9b959d445beead3a43a61ed6e3c4e82af41
Zeshan <zeshan-haider@hotmail.co.uk>
2023-04-11 01:53:10 +0500

    changed the task struct to a class and made its variables private

--- src/task.h
+++ src/task.h
@@ -10,19 +10,21 @@ BLANK SPACE FOR DOCUMENTATION LATER
 #include <string>
 #include <vector>
 
-struct Task {
-    std::string taskText;
-    int urgency;
-    int dueDate;
-    std::vector<std::string> tags;
-    bool completed;
+class Task {
+    private:
+        std::string taskText;
+        int urgency;
+        int dueDate;
+        std::vector<std::string> tags;
+        bool completed;
 
-    void editTaskText(std::string newTaskText);
-    void editTaskUrgency(int newUrgency);
-    void editTaskDueDate(int newDueDate);
-    void deleteTag(int tagIndex);
-    void addTag(std::string newTag);
-    void editCompletion(bool completion);
+    public:
+        void editTaskText(std::string newTaskText);
+        void editTaskUrgency(int newUrgency);
+        void editTaskDueDate(int newDueDate);
+        void deleteTag(int tagIndex);
+        void addTag(std::string newTag);
+        void editCompletion(bool completion);
 };
 
 #endif
