ec9d0bca5d087f9bc7a98b78e17dea1f9c341f1c
Zeshan <zeshan-haider@hotmail.co.uk>
2023-04-25 11:19:50 +0500

    added the interface for the project object, tests to be written next

---
+++ src/model/project.h
@@ -0,0 +1,49 @@
+/*
+
+BLANK SPACE FOR DOCUMENTATION LATER
+
+*/
+
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include <vector>
+#include <string>
+#include "task.h"
+#include "note.h"
+
+class Project {
+    private:
+        std::vector<Task> tasks;
+        std::vector<Note> notes;
+        std::string lastAction;
+        std::string description;
+
+    public:
+        Project(std::vector<Task> tasks, std::vector<Note> notes,
+                std::string lastAction, std::string description);
+        
+        // data manipulation methods
+        void addTask(Task task);
+        void editTask(int taskID);
+        void deleteTask(int taskID);
+
+        void addNote(Note note);
+        void editNote(int noteID);
+        void deleteNote(int noteID);
+
+        void changeLastAction(std::string lastActionText);
+
+        void editDescription(std::string descriptionText);
+
+        void readProjectData(std::string filename);
+        void writeProjectData(std::string filename);
+
+        // getter methods
+        std::vector<Task> returnTaskList();
+        std::vector<Note> returnNoteList();
+        std::string returnLastAction();
+        std::string returnDescription();
+};
+
+#endif
