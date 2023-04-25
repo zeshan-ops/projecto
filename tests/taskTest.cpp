589c9dd7634801a2b8f77b4ce2547010d4a9b66b
Zeshan <zeshan-haider@hotmail.co.uk>
2023-04-25 10:22:38 +0500

    grouped the task test cases into a test_suite

--- tests/taskTest.cpp
+++ tests/taskTest.cpp
@@ -2,37 +2,39 @@
 #include "doctest.h"
 #include "../src/model/task.h"
 
-TEST_CASE("Task Object Correct Initilisation Check") {
-    Task testTask("Initialisation Check", 0, 0);
-    std::string taskText = testTask.getTaskText();
-
-    CHECK(taskText.compare("Initialisation Check") == 0);
-    CHECK(testTask.getDueDate() == 0);
-    CHECK(testTask.getUrgency() == 0);
-    CHECK(!testTask.isCompleted());
-}
-
-TEST_CASE("Task Attribute Modifcation Check") { 
-    Task testTask("Test Task", 0, 0);
-
-    SUBCASE("Task Test Modification") {
-        testTask.editTaskText("Modified text");  
+TEST_SUITE("Task Object Tests") {
+    TEST_CASE("Task Object Correct Initilisation Check") {
+        Task testTask("Initialisation Check", 0, 0);
         std::string taskText = testTask.getTaskText();
-        CHECK(taskText.compare("Modified text") == 0);
+
+        CHECK(taskText.compare("Initialisation Check") == 0);
+        CHECK(testTask.getDueDate() == 0);
+        CHECK(testTask.getUrgency() == 0);
+        CHECK(!testTask.isCompleted());
     }
 
-    SUBCASE("Task Urgency Modifcation") {
-        testTask.editTaskUrgency(1);
-        CHECK(testTask.getUrgency() == 1);
-    }
+    TEST_CASE("Task Attribute Modifcation Check") { 
+        Task testTask("Test Task", 0, 0);
 
-    SUBCASE("Task Date Modification") {
-        testTask.editTaskDueDate(2);
-        CHECK(testTask.getDueDate() == 2);
-    }
+        SUBCASE("Task Test Modification") {
+            testTask.editTaskText("Modified text");  
+            std::string taskText = testTask.getTaskText();
+            CHECK(taskText.compare("Modified text") == 0);
+        }
 
-    SUBCASE("Task Completion Check") {
-        testTask.editCompletion(true);
-        CHECK(testTask.isCompleted());
+        SUBCASE("Task Urgency Modifcation") {
+            testTask.editTaskUrgency(1);
+            CHECK(testTask.getUrgency() == 1);
+        }
+
+        SUBCASE("Task Date Modification") {
+            testTask.editTaskDueDate(2);
+            CHECK(testTask.getDueDate() == 2);
+        }
+
+        SUBCASE("Task Completion Check") {
+            testTask.editCompletion(true);
+            CHECK(testTask.isCompleted());
+        }
     }
 }
