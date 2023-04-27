23586579cc8aa48dee34c9df83529a4ed0a9877a
Zeshan <zeshan-haider@hotmail.co.uk>
2023-04-27 18:09:47 +0500

    json datafile reading implemented, unit tests passing

--- src/model/txtDataUtility.cpp
+++ src/model/txtDataUtility.cpp
@@ -7,14 +7,47 @@ bool validateFile(std::string filename) {
 */
 
 json fileReader(std::string filename) {
-    std::ifstream file(filename);
-    json projectData = json::parse(file);
-    file.close();
+    std::ifstream f(filename);
+    json projectData = json::parse(f);
+    f.close();
 
     return projectData;
 }
 
-std::vector<Task> readTasks(json projectData);
-std::vector<Note> readNotes(json projectData);
-std::string readDescription(json projectData);
-std::string readLastAction(json projectData);
+std::vector<Task> readTasks(json projectData) {
+    std::vector<Task> storedTasks;
+
+    for(int i = 0; i < projectData["Tasks"].size(); i++) {
+        std::string text = projectData["Tasks"][i]["taskText"];
+        int taskUrg          = projectData["Tasks"][i]["taskUrg"];
+        int taskDue          = projectData["Tasks"][i]["taskDue"];
+        bool taskCompletion  = projectData["Tasks"][i]["taskComplete"];
+
+        Task storedTask(text,taskUrg,taskDue);
+
+        storedTasks.push_back(storedTask);
+    }
+
+    return storedTasks;
+}
+
+std::vector<Note> readNotes(json projectData) {
+    std::vector<Note> storedNotes;
+
+    for(int i = 0; i<projectData["Notes"].size(); i++) {
+        std::string noteText = projectData["Notes"][i]["noteText"];
+
+        Note storedNote(noteText);
+        storedNotes.push_back(storedNote);
+    }
+
+    return storedNotes;
+}
+
+std::string readDescription(json projectData) {
+    return projectData["Description"];
+}
+
+std::string readLastAction(json projectData) {
+    return projectData["Last Action"];
+}
--- tests/dataReadingTest.cpp
+++ tests/dataReadingTest.cpp
@@ -17,27 +17,29 @@ TEST_SUITE("Basic data reading test") {
         SUBCASE("Reading task data") {
             std::vector<Task> testTasks;
             Task task1("Task 1", 0, 0);
-            Task task2("Task 2", 1, 2);
-            Task task3("Task 3", 3, 20);
+            Task task2("Task 2", 0, 0);
+            Task task3("Task 3", 0, 0);
 
             testTasks.push_back(task1);
             testTasks.push_back(task2);
             testTasks.push_back(task3);
 
-            CHECK(readTasks(projectData) == testTasks);
+            std::vector<Task> projectTasks = readTasks(projectData);
+
+            CHECK(projectTasks == testTasks);
         }
 
         SUBCASE("Reading notes") {
             std::vector<Note> testNotes;
             Note note1("Note 1");
             Note note2("Note 2");
-            Note note3("Note 3");
 
             testNotes.push_back(note1);
             testNotes.push_back(note2);
-            testNotes.push_back(note3);
 
-            CHECK(readNotes(projectData) == testNotes);
+            std::vector<Note> projectNotes = readNotes(projectData);
+
+            CHECK(projectNotes == testNotes);
         }
 
         SUBCASE("Reading description and last action") {
