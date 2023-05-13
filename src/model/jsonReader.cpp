c12158f9b34076873ba4c7362b046d3162fa10cf
Zeshan <zeshan-haider@hotmail.co.uk>
2023-05-13 23:09:24 +0500

    adjusted jsonReader object to reflect better implementation of SRP

--- src/model/jsonReader.cpp
+++ src/model/jsonReader.cpp
@@ -1,104 +1,30 @@
 #include "jsonReader.h"
 
 ////////////////////////////////////////////////////////////////////////////////
-bool jsonReader :: fileValidater(std::string filename) const {
-    std::ifstream inputFile(filename);
-    
-    // Checking if the file can be opened
-    if(!inputFile.is_open()){
-        return false;
-    } else {
-        inputFile.close();
-        return true;
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-void jsonReader :: fileParser(std::string filename) {
+jsonReader :: jsonReader(std::string filename) {
     std::ifstream inputFile(filename);
     allProjectData = json::parse(inputFile);
+    inputFile.close();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-bool jsonReader :: projectVerifier(const std::string projectName) const {
-    for(auto& element : allProjectData) {
-        if (element["projectName"] == projectName) {
-            return true;
-        }
-    }
-
-    return false;
+json jsonReader :: getAllData() {
+    return allProjectData;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void jsonReader :: projectSelector(const std::string projectName) {
-    int projectIndex = -1;
-    for(auto& element: allProjectData) {
-        projectIndex += 1;
-        if(element["projectName"] == projectName) {
+json jsonReader :: getProjectData(std::string projectName) {
+    return allProjectData[getProjectIndex(projectName)];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+int jsonReader :: getProjectIndex(std::string projectName) {
+    int index = 0;
+    for(auto& project : allProjectData) {
+        if(project["projectName"] == projectName) {
             break;
         }
     }
-    selectedProject = allProjectData[projectIndex];
+    return index;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-std::vector<Task> jsonReader :: taskReader() const {
-    json dataTaskList = selectedProject["tasks"];
-    
-    std::vector<Task> taskList;
-    
-    for(auto& element : dataTaskList) {
-        Task newTask("Initial");
-        newTask.setText(element["text"]);
-        newTask.setDueDate(element["dueDate"]);
-        newTask.setCompleted(element["completed"]);
-        newTask.setUrgency(element["urgency"]);
-
-        taskList.push_back(newTask);
-    }
-
-    return taskList;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-std::vector<Log> jsonReader :: logReader() const {
-    json dataLogList = selectedProject["logs"];
-
-    std::vector<Log> logList;
-
-    for(auto& element : dataLogList) {
-        Log newLog("Initial", 0);
-        newLog.setText(element["text"]);
-        newLog.setTime(element["time"]);
-
-        logList.push_back(newLog);
-    }
-
-    return logList;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-std::string jsonReader :: nameReader() const {
-    return selectedProject["projectName"];
-}
-
-////////////////////////////////////////////////////////////////////////////////
-Project jsonReader :: createProject(std::string filename, 
-                                    const std::string projectName) {
-    fileParser(filename);
-    projectSelector(projectName);
-
-    Project project("Initial construction");
-
-    project.setName(nameReader());
-    project.setTasks(taskReader());
-    project.setLogs(logReader());
-
-    return project;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-json jsonReader :: returnAllData() const {
-    return allProjectData;
-}
--- src/model/jsonReader.h
+++ src/model/jsonReader.h
@@ -17,21 +17,14 @@ using nlohmann::json;
 class jsonReader {
     private:
         json allProjectData;
-        json selectedProject;
     public:
-        bool fileValidater(std::string filename) const;
-        void fileParser(std::string filename);
+        jsonReader(std::string filename);
 
-        bool projectVerifier(const std::string projectName) const;
-        void projectSelector(const std::string projectName);
-        
-        std::vector<Task> taskReader() const;
-        std::vector<Log> logReader() const;
-        std::string nameReader() const;
+        json getAllData();
+        json getProjectData(std::string projectName);
 
-        Project createProject(std::string filename, const std::string projectName);
-
-        json returnAllData() const;
+        // utility function
+        int getProjectIndex(std::string projectName);
 };
 
 #endif
