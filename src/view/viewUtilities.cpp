b015993f5f81907f487b7a94713263bceeffef73
Zeshan <zeshan-haider@hotmail.co.uk>
2023-05-30 06:17:52 +0500

    fixed the algorithms for the viewUtil functions, added more unit tests for each one too

--- src/view/viewUtilities.cpp
+++ src/view/viewUtilities.cpp
@@ -11,8 +11,8 @@ std::string viewUtil :: truncateText(std::string text, unsigned int truncLength)
     else {
         std::string truncatedString = text.substr(0,truncLength);
         for(unsigned int i = 0; i < truncatedString.length(); i++){
-            if(truncatedString[truncatedString.length()-i] == ' '){
-                return (truncatedString.substr(0,truncatedString.length()-i) + "...");
+            if(truncatedString[truncatedString.length()-3-i] == ' '){
+                return (truncatedString.substr(0,truncatedString.length()-3-i) + "...");
             } 
         }
         return truncatedString.substr(0,truncatedString.length()-3) + "...";
@@ -28,12 +28,32 @@ std::string viewUtil :: wrapText(std::string text, unsigned int lineLength) {
         return text;
     }
 
+    bool lastCharSpace = false;
     for(unsigned int i = 0; i < std::ceil(text.length()/lineLength); i ++) {
         for(unsigned int j = 0; j < lineLength; j++) {
-                if(text[(lineLength*(i+1)-j+1)] == ' '){
+            if(lastCharSpace) {
+                if(text[(lineLength*(i+1))-j+1] == ' ') {
                     text.at((lineLength*(i+1)-j+1)) = '\n';
+                    if(j==0){
+                        lastCharSpace = true;
+                    } else {lastCharSpace = false;}
                     break;
                 }
+            }
+
+            if(text[(lineLength*(i+1))-j] == ' ') {
+                text.at((lineLength*(i+1)-j)) = '\n';
+                if(j==0) {
+                    lastCharSpace = true;
+                }
+                break;
+            }
+
+            if(j == lineLength-1) {
+                text.insert((lineLength*(i+1))+i, "\n");
+                lastCharSpace = false;
+                break;
+            }
         }
     }
     return text;
--- tests/viewTests/viewUtilitiesTests.cpp
+++ tests/viewTests/viewUtilitiesTests.cpp
@@ -2,12 +2,16 @@
 #include "../../src/view/viewUtilities.h"
 
 TEST_SUITE("View Utilities Test: Truncate Text") {
-    TEST_CASE("Returning text without truncation") {
+    TEST_CASE("Trunc length > text length, so do not truncate") {
         CHECK(viewUtil::truncateText("Not truncated", 50) == "Not truncated");
     }
 
-    TEST_CASE("Returning text with truncation") {
-        CHECK(viewUtil::truncateText("This is going to be truncated", 15) == "This is going...");
+    TEST_CASE("Truncate below threshold") {
+        CHECK(viewUtil::truncateText("This is going to be truncated", 15) == "This is...");
+    }
+
+    TEST_CASE("Truncate at threshold") {
+        CHECK(viewUtil::truncateText("I want this text to truncate here", 19) == "I want this text...");
     }
 
     TEST_CASE("Not truncating if trunc length is zero") {
@@ -24,24 +28,34 @@ TEST_SUITE("View Utilities Test: Truncate Text") {
 }
 
 TEST_SUITE("View Utilities Test: Wrap Text") {
-    TEST_CASE("Wrap length > text length") {
-        CHECK(viewUtil::wrapText("The quick brown fox jumped over the lazy dog", 81) == "The quick brown fox jumped over the lazy dog");
-    }
-
-    TEST_CASE("Wrap length = text length") {
-        CHECK(viewUtil::wrapText("The quick brown fox jumped over the lazy dog", 80) == "The quick brown fox jumped over the lazy dog");
-    }
-
-    TEST_CASE("Wrap length < text length") {
-        std::string expectedOuput = "The quick brown\nfox jumped over\nthe lazy dog";
-        CHECK(viewUtil::wrapText("The quick brown fox jumped over the lazy dog", 15) == expectedOuput);
-    }
-
     TEST_CASE("Zero wrap length") {
         CHECK(viewUtil::wrapText("Blah blah blah", 0) == "");
     }
 
     TEST_CASE("Empty string input") {
         CHECK(viewUtil::wrapText("", 10) == "");
+    }
+    
+    TEST_CASE("Wrap length > text length, so do not wrap") {
+        CHECK(viewUtil::wrapText("The quick brown fox jumped over the lazy dog", 81) == "The quick brown fox jumped over the lazy dog");
+    }
+
+    TEST_CASE("Wrap length = text length, so do not wrap") {
+        CHECK(viewUtil::wrapText("The quick brown fox jumped over the lazy dog", 80) == "The quick brown fox jumped over the lazy dog");
+    }
+
+    TEST_CASE("Wrap at a non-space character") {
+        std::string expectedOuput = "The quick brown\nfox jumped over\nthe lazy dog";
+        CHECK(viewUtil::wrapText("The quick brown fox jumped over the lazy dog", 15) == expectedOuput);
+    }
+
+    TEST_CASE("Wrap at a space character") {
+        CHECK(viewUtil::wrapText("There is a space I want it to wrap at the space, will it do it?",17) ==
+               "There is a space\nI want it to wrap\nat the space,\nwill it do it?" );
+    }
+
+    TEST_CASE("No spaces wrapping") {
+        CHECK(viewUtil::wrapText("123456789123456789123456789123456789", 9) ==
+                "123456789\n123456789\n123456789\n123456789\n");
     }
 }
