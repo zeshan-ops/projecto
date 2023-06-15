10e16c3f8f91879cf54b76c60f10fdd5d864e26e
Zeshan <zeshan-haider@hotmail.co.uk>
2023-06-15 16:48:12 +0100

    Updated the main function to reflect the final envisioned program flow

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,34 @@ BLANK SPACE FOR DOCUMENTATION LATER
 
 */
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include "controller/cliParser.h"
+
 int main(int argc, char* argv[]){
+    std::vector<std::string> arguments;
     
+    if (argc > 1) {
+        std::vector<std::string> manyArgs (argv + 1, argv + argc);
+        arguments = manyArgs;
+    }
+
+    cliParser argParser(arguments);
+
+    if (argParser.commandExists()) {
+        command& cmd = argParser.getCmdObject();
+        if (cmd.verifyArgs()) {
+            std::cout << cmd.execute();
+            return 0;
+        } 
+        else {
+            std::cout << cmd.errorMessage();
+            return 0;
+        }
+    } 
+    else {
+        // print error message
+        return 0;
+    }
 }
