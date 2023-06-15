022b221a5ac7c68de963caf266a2f5b1f789ef1d
Zeshan <zeshan-haider@hotmail.co.uk>
2023-06-16 00:32:00 +0100

    Added more unit tests to the initialiser class

--- tests/controllerTests/initialiserTests.cpp
+++ tests/controllerTests/initialiserTests.cpp
@@ -2,9 +2,13 @@
 #include "../../src/controller/initialiser.h"
 
 #include <filesystem>
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 TEST_SUITE("Controller: initialiser Class Tests") {
-    TEST_CASE("Creating data directory when it does not exist") {
+    TEST_CASE("Creating data directory and init file when they do not exist") {
         initialiser initialiserTest;
 
         std::string dataPath = (std::string)std::getenv("HOME") + "/.projecto";
@@ -17,6 +21,26 @@ TEST_SUITE("Controller: initialiser Class Tests") {
 
         std::filesystem::remove_all(dataPath);
 
+        CHECK(!std::filesystem::exists(dataPath));
+    }
+
+    TEST_CASE("Reading init file when it does exist") {
+        std::string dataPath = (std::string)std::getenv("HOME") + "/.projecto";
+
+        std::filesystem::create_directory(dataPath);
+
+        std::string initFileText = "FOCUSED_PROJECT\nTest Project 1";
+        std::ofstream initDataFile(dataPath + "/initial.txt");
+
+        initDataFile << initFileText;
+        initDataFile.close();
+
+        initialiser initer;
+
+        CHECK(initer.getFocusedProjectName() == "Test Project 1");
+        CHECK(initer.focusedProjectSet() == true);
+
+        std::filesystem::remove_all(dataPath);
         CHECK(!std::filesystem::exists(dataPath));
     }
 }
